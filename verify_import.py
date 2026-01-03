3df01bfeaf2152a8d03ff77e90708c9218282325
TeslaQ1 <104022375+TeslaZXC@users.noreply.github.com>
2026-01-03 12:44:28 +0300

    Delete verify_import.py

--- verify_import.py
+++
@@ -1,7 +0,0 @@
-try:
-    from api.main import app
-    print("Success: App imported correctly")
-except ImportError as e:
-    print(f"Error: {e}")
-except Exception as e:
-    print(f"Exception: {e}")
